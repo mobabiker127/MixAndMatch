fd486dd98624f5bac70a66e0a268cae18a1227d4
Mo Babiker <mobabiker127@gmail.com>
2025-10-11 19:38:00 +0100

    Basic button logic for adding price to total

---
+++ app.js
@@ -0,0 +1,17 @@
+const noodles = document.getElementById("noodles");
+const bread = document.getElementById("bread");
+const milk = document.getElementById("milk");
+
+addPrice: (button) => {
+    switch(button) {
+        case(noodles):
+            // add price to total
+            break;
+        case(bread):
+            // add price to total
+            break;
+        case(milk):
+            // add price to total
+            break;
+    }
+}
